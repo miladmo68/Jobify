e29070d9ec1c730eb7622b89ca4db095827a83f9
miladmo68 <miladmo68@gmail.com>
2025-03-05 16:47:08 -0500

    first server

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-import { value } from "./test-named.js";
-import randomValue from "./test-default.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+import { nanoid } from "nanoid";
 
-console.log(value);
-console.log(randomValue);
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
+});
