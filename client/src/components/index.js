59e40f4e032285ae0819aa706d68e944441aeddf
miladmo68 <miladmo68@gmail.com>
2025-03-05 15:37:14 -0500

    Front done

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Logo } from "./Logo.jsx";
+export { default as FormRow } from "./FormRow.jsx";
+export { default as BigSidebar } from "./BigSidebar.jsx";
+export { default as Navbar } from "./Navbar.jsx";
+export { default as SmallSidebar } from "./SmallSidebar.jsx";
+export { default as NavLinks } from "./NavLinks.jsx";
+export { default as LogoutContainer } from "./LogoutContainer.jsx";
+export { default as ThemeToggle } from "./ThemeToggle.jsx";
---
+++ server.js
@@ -0,0 +1,5 @@
+import { value } from "./test-named.js";
+import randomValue from "./test-default.js";
+
+console.log(value);
+console.log(randomValue);
