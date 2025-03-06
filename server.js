49ff7fa3cec85378b369ba02a7a484c0c1fec123
miladmo68 <miladmo68@gmail.com>
2025-03-06 08:54:43 -0500

    CRUD DONE on Server

--- server.js
+++ server.js
@@ -16,20 +16,75 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+// GET HELLO
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
+// CREATE HELLO MESSAGE
 app.post("/", (req, res) => {
   console.log(req);
-
   res.json({ message: "Data received", data: req.body });
 });
 
+// GET ALL JOBS
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
 
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+// CREATE JOB
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  // console.log(id);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
+});
+
+// EDIT JOB
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job modified", job });
+});
+
+// DELETE JOB
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: "job deleted" });
+});
+
+// SERVER
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
   console.log(`server running on PORT ${port}....`);
